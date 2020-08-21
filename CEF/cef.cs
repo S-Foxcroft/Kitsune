e5a963e915ee3fddefa0025077ad4e00fb088947
Shaun Foxcroft <sfoxcroft@outlook.com>
2020-08-21 17:45:32 +0100

    Implemented encrypt/decrypt and test method in CEF.

--- CEF/cef.cs
+++ CEF/cef.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Kitsune.Cryptography;
 
 namespace Kitsune
 {
@@ -10,6 +10,44 @@ namespace Kitsune
     {
         static void Main(string[] args)
         {
+            //check for default keys and generate if missing
+            string myPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            CryptoHandler kCrypto = new CryptoHandler();
+            if (!Directory.Exists(Path.Combine(myPath, "registered_keys"))) Directory.CreateDirectory(Path.Combine(myPath, "registered_keys"));
+            if (!Directory.Exists(Path.Combine(Path.GetTempPath(), "Kitsune"))) Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), "Kitsune"));
+            if (!File.Exists(Path.Combine(myPath, "registered_keys\\sanitycheck.kkr")))
+                using (StreamWriter w = new StreamWriter(File.Open(Path.Combine(myPath, "registered_keys\\sanitycheck.kkr"), FileMode.Create)))
+                    w.Write("g949d85b34722305\rdr25978sdg90d08n\r8er03h24j09oj5eg\rcheapkeyringtest\r"+
+                            "g949d85b34722305\rdr25978sdg90d08n\r8er03h24j09oj5eg\rcheapkeyringtest");
+            if(args.Length > 0)
+                if (args[0].Equals("--test",StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Test flag recieved, writing test file.");
+                    string testContents = "This is a test text document.\r\nIt's purpose is to confirm that the program is operating normally.\r\n"+
+                                          "When the program is executed with the test flag (--test), it will write this file then encrypt and decrypt it.\r\n"+
+                                          "If the resulting document does not equal this one, an issue has occured in the process.";
+                    using (StreamWriter w = new StreamWriter(File.Open(Path.Combine(Path.GetTempPath(), @"Kitsune\TEMP.txt"), FileMode.Create))) w.Write(testContents);
+                    //encrypt test file
+                    kCrypto.LoadKeyring(Path.Combine(myPath, "registered_keys\\sanitycheck.kkr"));
+                    Console.WriteLine("Keyring loaded");
+                    string encFile = kCrypto.EncryptFile(Path.Combine(Path.GetTempPath(), @"Kitsune\TEMP.txt"));
+                    Console.WriteLine("Test file encrpyted at "+encFile);
+                    string result = kCrypto.DecryptFile(encFile);
+                    //compare contents with test string.
+                    Console.WriteLine("Original\r\n========\r\n" + testContents);
+                    Console.WriteLine("\r\nResult\r\n========\r\n" + result);
+                    Console.WriteLine("\r\n\r\nTest " + ((result == testContents) ? "PASSED" : "FAILED"));
+                }
+                else
+                {
+                    Console.WriteLine("Program achieved nothing! Press return to exit.");
+                    Console.ReadLine();
+                }
+            else
+            {
+                Console.WriteLine("Program achieved nothing! Press return to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
--- crypto/Cryptography.cs
+++ crypto/Cryptography.cs
@@ -1,7 +1,98 @@
-﻿namespace Kitsune
-{
-    public class Cryptography
-    {
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
 
+namespace Kitsune.Cryptography
+{
+
+    public class CryptoHandler
+    {
+        private string[] Keyring = new string[8];
+        private static readonly string FileRegEx = @"^(.*)\.enc([0-8])";
+        private Random rand;
+        private RijndaelManaged rijn;
+        public CryptoHandler()
+        {
+            rijn = new RijndaelManaged
+            {
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7,
+                KeySize = 0x80,
+                BlockSize = 0x80
+            };
+            rand = new Random();
+        }
+        public void LoadKeyring(string filename)
+        {
+            try
+            {
+                string[] contents;
+                using (StreamReader r = new StreamReader(File.OpenRead(filename)))
+                    contents = r.ReadToEnd().Replace("\r\n", "|").Replace("\r","|").Split('|');
+                Array.Copy(contents, Keyring, contents.Length > 8 ? 8 : contents.Length);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("The file specified is not a valid Kitsune keyring.","filename", e);
+            }
+        }
+        public string EncryptFile(string filename)
+        {
+            //set up the encryptor
+            int keyIndex = rand.Next(1, 8);
+            string targetFile = filename+".enc"+keyIndex;
+            SelectKey(keyIndex);
+            ICryptoTransform ct = rijn.CreateEncryptor();
+            string data = Read(filename);
+
+            byte[] dataAsBytes = Encoding.UTF8.GetBytes(data);
+            data = Convert.ToBase64String(ct.TransformFinalBlock(dataAsBytes, 0, dataAsBytes.Length));
+            Write(targetFile, data);
+            return targetFile;
+        }
+        public string DecryptFile(string filename)
+        {
+            Match m = Regex.Match(filename, FileRegEx, RegexOptions.IgnoreCase);
+            if (m.Success)//eg C:\Users\Administrator\Documents\myFile.txt.enc7 -> "C:\...\myFile.txt" and "7"
+            {
+                string targetFile = m.Groups[1].Value;
+                SelectKey(Int32.Parse(m.Groups[2].Value)); //throws FormatException if not a number (should be 1-8 if the regex matches)
+                ICryptoTransform ct = rijn.CreateDecryptor();
+                string data = Read(filename);
+                byte[] dataAsBytes = Convert.FromBase64String(data);
+                data = Encoding.UTF8.GetString(ct.TransformFinalBlock(dataAsBytes, 0, dataAsBytes.Length));
+                Write(targetFile, data);
+                return data;
+            }
+            else throw new IOException("The file specified is not a Kitsune encrypted document.");
+        }
+        private string Read(string filename)
+        {
+            string data = "";
+            using (StreamReader r = new StreamReader(File.Open(filename, FileMode.Open)))
+            {
+                data = r.ReadToEnd();
+            }
+            if (data == "") throw new IOException("The file specified could not be read or is empty.");
+            return data;
+        }
+        private void Write(string filename, string data)
+        {
+            using (StreamWriter w = new StreamWriter(File.Open(filename, FileMode.Create)))
+            {
+                w.Write(data);
+            }
+        }
+        private void SelectKey(int keyIndex)
+        {
+            byte[] pass = Encoding.UTF8.GetBytes(Keyring[keyIndex - 1]);
+            byte[] ekBytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            Array.Copy(pass, ekBytes,
+                       pass.Length > ekBytes.Length ? ekBytes.Length : pass.Length);
+            rijn.Key = ekBytes;
+            rijn.IV = ekBytes;
+        }
     }
 }

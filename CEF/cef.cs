47118d70f9f3a866e1d325b700d09bca5ed3a495
Shaun Foxcroft <sfoxcroft@outlook.com>
2020-08-21 18:27:04 +0100

    Added argument identification and adding packaging. TODOs added for future development.

--- CEF/cef.cs
+++ CEF/cef.cs
@@ -8,6 +8,10 @@ namespace Kitsune
 {
     class CEF
     {
+        private enum ArgumentState
+        {
+            NONE, I_FILE, I_DIRECTORY, O_DIRECTORY
+        }
         static void Main(string[] args)
         {
             //check for default keys and generate if missing
@@ -19,7 +23,7 @@ namespace Kitsune
                 using (StreamWriter w = new StreamWriter(File.Open(Path.Combine(myPath, "registered_keys\\sanitycheck.kkr"), FileMode.Create)))
                     w.Write("g949d85b34722305\rdr25978sdg90d08n\r8er03h24j09oj5eg\rcheapkeyringtest\r"+
                             "g949d85b34722305\rdr25978sdg90d08n\r8er03h24j09oj5eg\rcheapkeyringtest");
-            if(args.Length > 0)
+            if (args.Length > 0)
                 if (args[0].Equals("--test",StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine("Test flag recieved, writing test file.");
@@ -40,11 +44,80 @@ namespace Kitsune
                 }
                 else
                 {
-                    Console.WriteLine("Program achieved nothing! Press return to exit.");
-                    Console.ReadLine();
+                    bool extractMode = false, copyKeyring = false;
+                    List<String> files = new List<String>(), dirs = new List<String>();
+                    string output = "";
+                    ArgumentState state = ArgumentState.NONE;
+                    for(int i = 0; i < args.Length; i++)
+                    {
+                        switch (args[i].ToLower())
+                        {
+                            case "/f":
+                            case "-f":
+                            case "/file":
+                            case "-file":
+                            case "/files":
+                            case "-files":
+                                state = ArgumentState.I_FILE;
+                                break;
+                            case "/d":
+                            case "-d":
+                            case "/dir":
+                            case "-dir":
+                            case "/directory":
+                            case "-directory":
+                                state = ArgumentState.I_DIRECTORY;
+                                break;
+                            case "/o":
+                            case "-o":
+                            case "/out":
+                            case "-out":
+                            case "/output":
+                            case "-output":
+                                state = ArgumentState.O_DIRECTORY;
+                                break;
+                            case "/e":
+                            case "-e":
+                            case "/extract":
+                            case "-extract":
+                                extractMode = true;
+                                state = ArgumentState.I_FILE;
+                                break;
+                            case "/i":
+                            case "-i":
+                            case "/includekey":
+                            case "-includekey":
+                                copyKeyring = true;
+                                break;
+                            default:
+                                //do things here depending on current state
+                                if (state == ArgumentState.I_FILE) files.Add(args[i]);
+                                else if (state == ArgumentState.I_DIRECTORY) dirs.Add(args[i]);
+                                else if (state == ArgumentState.O_DIRECTORY) output = Path.GetDirectoryName(args[i]);
+                                break;
+                        }
+                    }
+                    if (extractMode)
+                    {
+                        //TODO: decompress and decrypt
+                        //1. extract archive to destination
+                        //2. Identify the keyring and register it if not already present.
+                        //3. decompress the files
+                        //4. remove the extracted encrypted versions of each file.
+                        //5. remove the manifest.
+                    }
+                    else
+                    {
+                        //TODO: encrypt and compress
+                        //1. recreate folder structure
+                        //2. encrypt each file and move it to the new structure
+                        //3. compress the new structure
+                        //4. move archive to destination folder
+                    }
                 }
             else
             {
+                //TODO: Create a help page to show here
                 Console.WriteLine("Program achieved nothing! Press return to exit.");
                 Console.ReadLine();
             }
